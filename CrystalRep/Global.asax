f4b41915f68e7650b12ca32e336d43ddd1c03c6f
Mahmoud <mahmoudmedhat272@gmail.com>
2019-12-18 13:46:06 +0200

    Report

---
+++ CrystalRep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrystalRep.MvcApplication" Language="C#" %>
---
+++ CrystalRep/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Web;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrystalRep
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalReport1 rprt = new CrystalReport1();
+            rprt.Load(Server.MapPath("~/CrystalReport1.rpt"));
+            SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-E1MUI69; initial catalog = Northwind; integrated security = true;");
+            SqlCommand cmd = new SqlCommand("Proc_Product", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            rprt.SetDataSource(dt);
+            //rprt.SetParameterValue(0, Name);
+            CrystalReportViewer1.ReportSource = rprt;
+            CrystalReportViewer1.DataBind();
+
+
+        }
+
+
+
+
+
+
+    }
+}
